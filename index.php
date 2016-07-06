1e6e4da714946b5bb3ed718a370905f10f2c7574
as7bzx92dd <g04032016@mail.ru>
2016-07-06 14:38:36 +0300

    start php labs

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>
+        php
+    </title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 06.07.2016
+ * Time: 14:30
+ */
+echo "Hello, World";
+?>
+</body>
+</html>
