7e72e3498e3dc90c7fc96298b96acde88f269dbf
as7bzx92dd <g04032016@mail.ru>
2016-07-06 15:58:47 +0300

    variables

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- I repeat PHP -->
 <html>
 <head>
     <meta charset="UTF-8">
@@ -6,21 +7,27 @@
     </title>
 </head>
 <body>
-<?php
-echo "Hello, World";
-echo "<br />";
-$test = Date("d.m.y");
-echo "<br />";
-print "Еще один тест";
-echo "<br />";
-echo $test;
-echo $text= "Тело цикла";
-for($i=1;$i<10;$i++)
-{
-    echo $text."</br>";
-    echo "";
-};
-?>
+
+        <?php
+        echo "Hello, World";
+        echo "<br />";
+        $test = Date("d.m.y");
+        echo "<br />";
+        print "Еще один тест";
+        echo "<br />";
+        echo $test;
+        echo $text= "Тело цикла";
+            for($i=1;$i<10;$i++)
+            {
+                echo $text."</br>";
+                echo "";
+            };
+        if (isset($i)) echo "Существует"."</br>";
+        else echo "Не существует"."</br>";
+        unset($i);
+        if (isset($i)) echo "Существует"."</br>";
+        else echo "Не существует"."</br>";
+        ?>
 <p>Test</p>
 
 </body>
