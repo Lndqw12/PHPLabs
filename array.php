8bcc1e5ed095d9a48a0d878caf6f4bcefcbf5c8d
as7bzx92dd <g04032016@mail.ru>
2016-07-12 11:56:00 +0300

    array example 3

--- array.php
+++ array.php
@@ -3,7 +3,13 @@ $a = array();
 $a[0] = 21;
 $a[1] = 23;
 $a[2] = 25;
+//unset($a);
+$a[] = "example";
+$a[] = "example3";
+$a[] = "example4";
+$a[] = "example5";
 for ($i=0;$i<=count($a);$i++){
     echo $a[$i]."<br />";
 }
+echo count($a);
 ?>
