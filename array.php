1d548903a4d8057cd852c36d7fb62d43501311ae
as7bzx92dd <g04032016@mail.ru>
2016-07-12 12:06:15 +0300

    list

--- array.php
+++ array.php
@@ -1,15 +1,14 @@
+<meta charset="UTF-8">
 <?php
-$a = array();
-$a[0] = 21;
-$a[1] = 23;
-$a[2] = 25;
-//unset($a);
-$a[] = "example";
-$a[] = "example3";
-$a[] = "example4";
-$a[] = "example5";
-for ($i=0;$i<=count($a);$i++){
-    echo $a[$i]."<br />";
-}
-echo count($a);
+
+echo 'Текущая версия PHP: ' . phpversion();
+
+$array_list = array(
+    "start",
+     "begin",
+    "end"
+);
+
+list ( $start, $begin, $end) = $array_list;
+echo $start, $begin, $end;
 ?>
