8dee3b18ae6820d389effb159b16cb0100a5a0eb
as7bzx92dd <g04032016@mail.ru>
2016-07-13 17:41:24 +0300

    for array

--- arr3.php
+++ arr3.php
@@ -12,3 +12,16 @@
         echo "{$arrexample[$i]['name']}  was born {$arrexample[$i]['Bt']}<br/>";
     }
 ?>
+
+<?php
+    $bird = array(
+        "Thomas Anders" => "song",
+        "Mihel Jekson" => "soul",
+        "Mihel Jekson2" => "soul2"
+    );
+ for(reset($bird);($k = key($bird));next($bird))
+     echo "$k === {$bird[$k]}<br>";
+echo "---------------------------------------<br>";
+for(end($bird);($ggg = key($bird));prev($bird))
+    echo "$ggg === {$bird[$ggg]}<br>";
+?>
