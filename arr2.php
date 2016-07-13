cabb408a531f59382a54bebd782548702e52d8fe
as7bzx92dd <g04032016@mail.ru>
2016-07-13 16:57:41 +0300

    array and array

---
+++ arr2.php
@@ -0,0 +1,17 @@
+<meta charset="UTF-8">
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 13.07.2016
+ * Time: 16:49
+ */
+$qarr = array(
+    "one" => array("p"=>"n","h"=>"f"),
+    "two" => array("y"=>"7")
+);
+print_r($qarr);
+echo "<br />";
+echo $qarr['two'];
+print_r($qarr['two']);
+?>
