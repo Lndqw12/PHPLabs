6507f5ee1408dfda5f6f85277ad6a9fe515cc32a
as7bzx92dd <g04032016@mail.ru>
2016-07-13 19:14:47 +0300

    for and foreach

---
+++ foreach.php
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+<?php
+foreach(array(101,203,34) as $magic)
+    echo "On well ===>>> $magic<br>";
+    echo"----------------<br>";
+    $garr = array(34,56,78,5,0,88,33,22,34);
+        foreach($garr as $lol){
+            $lol++;
+            echo "$lol<br>";
+        }
+echo"----------------<br>";
+    for($h=0;$h<count($garr);$h++)
+        echo "$garr[$h]<br>";
+?>
