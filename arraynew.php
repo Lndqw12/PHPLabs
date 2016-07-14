a3110fb0334df9487bb0ffb7618e060fa30d1518
as7bzx92dd <g04032016@mail.ru>
2016-07-14 16:58:58 +0300

    return

--- arraynew.php
+++ arraynew.php
@@ -1,10 +1,31 @@
 <meta charset="UTF-8">
 <?php
+var_dump(arraynull());
+echo "<br/>";
+var_dump(setarray());
 function setarray(){
    return $duble = array(2,6,7);
 }
-$f = setarray();
-print_r($f);
+print_r(setarray());
+list($a1,$a2,$a3) = setarray();
+echo $a1,$a2,$a3;
+echo "<br/>";
+var_dump($a1);
+// null array
+function arraynull(){};
+echo "<br/>";
+var_dump(arraynull());
+function sum($sedik, $ararat = "4"){
+    $d= $sedik+$ararat;
+    return $d;
+}
+print sum(2);
+print sum(2,8);
 
+function sd($y){
+     return $y++;
+    echo $y;
+}
 
+print (sd(10));
 ?>
