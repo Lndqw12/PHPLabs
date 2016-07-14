1548b713aaaf2d1bcd88457be1b3218e54ede996
as7bzx92dd <g04032016@mail.ru>
2016-07-14 14:29:53 +0300

    function

---
+++ arraynew.php
@@ -0,0 +1,10 @@
+<meta charset="UTF-8">
+<?php
+function setarray(){
+   return $duble = array(2,6,7);
+}
+$f = setarray();
+print_r($f);
+
+
+?>
--- serialize.php
+++ serialize.php
@@ -6,4 +6,12 @@ print_r($ser);
 echo "<br/>";
 print_r("$ser");
 echo "<br/>";
+$nesarr = array(23,45,87);
+$alt = serialize($nesarr);
+echo "$alt";
+echo "---------------<br/>";
+$moto = unserialize($alt);
+
+print_r($moto);
+echo "<br/>";
 ?>
