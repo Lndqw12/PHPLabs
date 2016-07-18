7f777d1d1adda4d993c3f8a2b732a642af64f490
as7bzx92dd <g04032016@mail.ru>
2016-07-18 10:50:59 +0300

    global func array &

--- labsfun.php
+++ labsfun.php
@@ -40,5 +40,54 @@ function globFunArray(){
 
 echo "Последний<br/>";
 echo globFunArray();
+print("<br/>");
+print("--------------------");
+$test= 77;
+function dlop(){
+    global $test;
+    echo "$test";
+}
+$r = 99;
+function dlop2(){
+    $r= $GLOBALS["test"];
+    echo $r;
+    unset($r);
+    $r = 34;
+    echo $r;
+}
+print("<br/>");
 
+dlop();
+print("<br/>");
+
+dlop2();
+print("<br/>");
+
+function deler(){
+    unset($GLOBALS["test"]);
+    echo $fg;
+}
+deler();
+print("---------------");
+echo "$test";
+$as= 23;
+function dum1(){
+    global $as; // тут меняься содержимое глобальное переменной, ибо это ссылка а не компия
+    $as = 45;
+    echo $as;
+}
+print("---------------<br/>");
+ dum1();
+echo "$as<br/>";
+// следующий эксперемент через глобальную функцию переменыых
+$df = 67;
+function dum2(){
+    $qqq = &$GLOBALS['df'];
+    $qqq = 200;
+    echo $qqq;
+}
+echo "<br/>";
+dum2(); // 200
+echo "$df"; // 67почему??? да потому что через ссылку из функции произвели перезапись переменной новым
+// значением
 ?>
