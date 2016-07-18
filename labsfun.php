189466765dad06269c8625419917c0826ea9017e
as7bzx92dd <g04032016@mail.ru>
2016-07-18 09:31:01 +0300

    global var

---
+++ labsfun.php
@@ -0,0 +1,32 @@
+<meta charset="UTF-8">
+<?php
+function myecho(){
+    for($i=0;$i<func_num_args();$i++) {
+        echo func_get_arg($i)."<br>\n";
+    }
+}
+ myecho("asd","asdgha","weiuy","weiweoi");
+for($e=0;$e<20;$e++)
+echo "--";
+echo "<br/>";
+
+function myecho2(){
+    foreach (func_get_args() as $k){
+        echo "$k<br>\n";
+    }
+}
+myecho2("asdasdada","asdghasdasda","wewreiuy","wei3434weoi");
+
+for($e=0;$e<20;$e++)
+    echo "--";
+echo "<br/>";
+
+$f= 20;
+function localVar(){
+    global  $f;  // сюда пишем что обращение идет к глобальной переменной
+    echo "$f<br/>";
+    return $f;
+}
+echo "$f";
+echo localVar();
+?>
