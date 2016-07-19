3dda32f3f03cd37be1a26eee53e5a0086e9899b6
as7bzx92dd <g04032016@mail.ru>
2016-07-19 17:06:10 +0300

    link and call_user_func

--- funsilk.php
+++ funsilk.php
@@ -4,5 +4,8 @@ function A($i) {echo "Вызвана A($i)\n";}
 function B($i) {echo "Вызвана B($i)\n";}
 function C($i) {echo "Вызвана C($i)\n";}
 $F="A";
+$H="B";
 $F(304);
+call_user_func($F,47);
+call_user_func($H,532);
 ?>
