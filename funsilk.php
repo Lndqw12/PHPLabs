8b88523301ff9f833ba960c45513fd62b9b946dc
as7bzx92dd <g04032016@mail.ru>
2016-07-19 16:59:55 +0300

    link

---
+++ funsilk.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<?php
+function A($i) {echo "Вызвана A($i)\n";}
+function B($i) {echo "Вызвана B($i)\n";}
+function C($i) {echo "Вызвана C($i)\n";}
+$F="A";
+$F(304);
+?>
--- virtual.php
+++ virtual.php
@@ -1,10 +1,12 @@
 <meta charset="UTF-8">
 <?php
-if (!function_exists("virtual")){
-    function virtual($uri){
-        $url = "http://".$_SERVER["HTTP_HOST"].$uri;
-        echo join("", file($url));
-    }
-}
-virtual("/");
+//if (!function_exists("virtual")){
+//    function virtual($uri){
+//        $url = "http://".$_SERVER["HTTP_HOST"].$uri;
+//        echo join("", file($url));
+//    }
+//}
+//virtual("/");
+
+echo $_SERVER["HTTP_HOST"]; // вернул localhost:63342
 ?>
