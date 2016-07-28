f57a07725014698563766daf4f68815293fea97e
as7bzx92dd <g04032016@mail.ru>
2016-07-28 09:55:50 +0300

    string cmp

--- chartable.php
+++ chartable.php
@@ -1,22 +1,52 @@
 <!--<meta charset="UTF-8">-->
+<!--BASIC FUNCTION FOR WORK STRING-->
 <?php
-  for($i=0,$x=0;$x<16;$x++){
-      for($y=0;$y<16;$y++){
-          $chars[$x][$y] = array($i,chr($i));
-          $i++;
-      }
-  }
-?>
-<table border="1" cellpadding="1" cellspacing="0">
-    <?foreach ($chars as $row){?>
-        <tr>
-            <?foreach ($row as $cell){ ?>
-            <td>
-                <?=$cell[0]?>:
-                <b><tt><?=$cell[1]?></tt></b>
-            </td>
-            <?}?>
-        </tr>
-    <?}?>
 
-</table>
+$str1= "asdgjkasdfasdfasdf";
+$str2= "21371273087123asdfasdf";
+
+$str3 = "asd";
+
+$result = strpos($str2,$str1);
+if ($result===false)
+    print "No";
+//echo $result;
+$result = strpos($str2,$str3);
+if ($result===false)
+    print "No";
+else {
+    print "Yes";
+    print $result;
+}
+// next expample
+$resultnewpos = strpos($str2,$str3,15);// witch position 15 It have to been number 18
+if(!($resultnewpos===false))
+    print "<br/>".$resultnewpos;
+//last position
+
+$resullastpos = strrpos($str2,$str3);
+if (!($resullastpos===false))
+print "<br/>".$resullastpos;
+
+// basic function strcamp
+$str5 = "qwoeiyqiowuHKJHJKHJK8979790";
+$str6 = "qwyyeqw";
+$str7 = "sadfjaskdfasldfhsfuerqwuncb;ernuwqpefinqwecnwioqefoqwef";
+$resulthis = strcmp($str6,str5);
+campi($resulthis);
+// function for definition < or > , maybe even FULL
+function campi($var){
+
+switch ($var) {
+    case 0:
+        echo "full";
+        break;
+    case 1:
+        echo "<br/>"."first string  MORE second string";
+        break;
+    case -1:
+        echo "<br/>"."first string LESS second string";
+        break;
+}
+}
+?>
