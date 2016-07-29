b0b08a813220a1d22658c369ea386553a03e3a15
as7bzx92dd <g04032016@mail.ru>
2016-07-29 09:06:28 +0300

    sort array

---
+++ arraysort.php
@@ -0,0 +1,18 @@
+<?php
+$tools = array(
+    "a"=>"adsf",
+    "b"=>"2434",
+    "c"=>"popi",
+    "d"=>"jhg"
+);
+
+asort($tools);
+print_r($tools);
+echo "<br/>";
+echo $tools;
+echo "<br/>";
+print $tools;
+arsort($tools);
+echo "<br/>";
+print_r($tools);
+?>
