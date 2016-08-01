2d3c868db3354c3f056c3289f7486b4376ac59c5
as7bzx92dd <g04032016@mail.ru>
2016-08-01 10:49:40 +0300

    fun_array2

--- array/arrstone.php
+++ array/arrstone.php
@@ -1,7 +1,21 @@
 <?php
+echo "<pre>";
 $as = "qwiyeio";
 $fg = "fgh";
 $ui = "to89";
-$A = compact("as","fg");
+//$list = array("fg32434"=>"adsaas","hj2323"=>"klhjkl");
+$A = compact("as","fg",$list);
 print_r($A);
+print($A);
+$aaa = array_push($A,"sel");
+print_r($A);
+array_pop($A);
+print_r($A);
+$newvar = array_unshift($A,23);
+echo "$newvar";
+print_r($A);
+$vol = array_shift($A);
+print_r($A);
+echo "$vol";
+echo "</pre>";
 ?>
