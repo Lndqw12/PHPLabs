ce7dcad5a5f1934cf3a6cd52d5cf31a6e23b7c84
as7bzx92dd <g04032016@mail.ru>
2016-08-05 09:10:54 +0300

    file

--- array/ffopen.php
+++ array/ffopen.php
@@ -1,14 +1,27 @@
+<meta charset="UTF-8">
 <?php
 $ph = $_SERVER['DOCUMENT_ROOT'];
 echo "$ph";
-$file = "$ph"."/"."random.php";
+$file = "random.php";
 echo "<br />";
 echo "$file";
 $fstyl = "rt";
 //$f = fopen($file,$fstyl) or die("Error");
-$f1 = fopen("random.php","rt") or die("Error1");
-echo "<pre>";
+$f = fopen($file,"rt") or die("Error1");
+echo "<br />";
+echo is_array($f) ? 'Массив' : 'Не массив';
+echo "<br />";
+echo is_object($f) ? 'Объект' : 'Не объект';
+echo "<br />";
+echo is_string($f) ? 'Строка' : 'Не строка';
+echo "<br />";
+echo "-------------------------------------<br/>";
+function mHex($input){
+    for($i=0;$i<strlen($input);$i++)
+        $hex[] = sprintf("%20X",ord($input[$i]));
+    return join(" ",$hex);
+}
+echo mHex(fgets($f,100));
 
 
-echo "</pre>";
 ?>
