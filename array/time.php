0f342cceb41245c5b666be959d083cf0dccc62db
as7bzx92dd <g04032016@mail.ru>
2016-08-09 10:30:02 +0300

    date_and_time2

--- array/time.php
+++ array/time.php
@@ -10,4 +10,12 @@ echo "<br/>";
 for($i=0;$i<1000;$i++)
     $i++;
 printf("Время работы скрипта %.5f c",microtime(true)-START_TIME );
+echo "<br/>";
+$mctime = microtime(true);
+printf("Unix started = %f sec.<br>", $mctime);
+echo "Unix started = $mctime sec.<br>";
+echo "<br/>";
+echo date("l dS of F Y h:i:s A")."<br>";
+echo date("d.m.Y")."<br>";
+echo date("d.m.Y",filectime(__FILE__));
 ?>
