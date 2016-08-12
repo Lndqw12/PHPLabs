2b2fd0e655fa1538e4e4078211fc3c2d497f4a97
as7bzx92dd <g04032016@mail.ru>
2016-08-12 15:09:47 +0300

    error

--- array/inter.php
+++ array/inter.php
@@ -4,5 +4,30 @@ echo "<pre>";
 echo phpversion();
 echo "<br/>";
 echo "Last chenge = ".date("d.m.Y H:i.s.",getlastmod());
+echo "<br/>";
+echo $DOCUMENT_ROOT;
+echo $_SERVER['DOCUMENT_ROOT'];
 echo "</pre>";
+
+echo "----------------------------";
+echo "<br/>";
+
+error_reporting(E_ALL);
+ini_set("error_log", "log.txt");
+ini_set("log-errors", true);
+//filetime("spoon");
+echo "----------------------------";
+echo "<br/>";
+
+function inner ($a) {
+    echo "<pre>";
+    print_r(debug_backtrace());
+    echo "<pre/>";
+}
+
+function outer ($x) {
+    inner ($x*$x);
+}
+
+outer(3);
 ?>
