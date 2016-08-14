636ecfa3d1c2077dd645766ec7682670053d8750
as7bzx92dd <g04032016@mail.ru>
2016-08-14 19:53:51 +0300

    regE

--- array/eval.php
+++ array/eval.php
@@ -3,14 +3,24 @@
 //printf("uniqid(): %s\r\n", uniqid("oiouiouo"));
 $squarers = array();
 //for ($i=0;$i<=10;$i++){
-    $id = uniqid("F");
-$txt = "OK";
-$st = "function $id() { echo $txt; }";
+    $id = uniqid("d");
+    $txt = "OK";
+    $st = "function $id() { echo $txt; }";
     echo $st;
     echo "<br/>";
     eval($st);
     $squarers[] = $id;
 //}
 $squarers[0]();
+echo "<br/>";
 
+for ($k=0;$k<=10;$k++){
+    echo uniqid("$k");
+    echo "<br/>";
+}
+echo "<br/>";
+$mul = create_function('$a,$b','return $a*$b;');
+$neg = create_function('$a','return -$a;');
+echo $mul(10,20)."<br>";
+echo $neg(2)."<br>";
 ?>
