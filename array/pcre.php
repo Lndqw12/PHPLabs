b533647de7f295a05be32cdd746137277774de1d
as7bzx92dd <g04032016@mail.ru>
2016-08-15 12:31:29 +0300

    pcra1

--- array/pcre.php
+++ array/pcre.php
@@ -1,4 +1,16 @@
+<meta charset="UTF-8">
 <?php
 preg_match('/(\d+)/s',"articl_123.html", $pockets);
 echo $pockets[1];
+echo "<br/>";
+preg_match('/(\S+)@([a-z0-9.]+)/is',"Привет от somebody@mail.ru", $p);
+echo "В тексте найдено = $p[1], хост - $p[2]";
+echo "<br/>";
+$text = "Привет от somebody@mail.ru, а также от other@mail.ru";
+$html = preg_replace(
+    '/(\S+)@([a-z0-9.]+)/is',
+    '<a href = "mailto:$0>$0</a>',
+    $text
+);
+echo $html;
 ?>
